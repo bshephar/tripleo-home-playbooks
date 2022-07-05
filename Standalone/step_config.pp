88f5d36ff4c09b5daffb60aa1752043c8b1f75e0
stack <stack@tripleo-director.bne-home.net>
2022-07-04 23:51:54 -0400

    Automatic commit with fresh config download

---
+++ Standalone/step_config.pp
@@ -0,0 +1,8 @@
+include tripleo::trusted_cas
+include tripleo::profile::base::database::mysql::client
+include tripleo::profile::base::pacemaker
+
+
+include tripleo::profile::base::snmp
+
+include tripleo::packages
